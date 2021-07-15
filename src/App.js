65b8f963f4e3a0ed477d4360892d6c69ce4a4953
TimZoe <contact@trophie.de>
2021-07-15 09:20:04 +0200

    f

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-const App = () => (
-  <div>
-    <AmplifySignOut />
-    My App
-  </div>
-);
 
-export default withAuthenticator(App);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Trophie1507 mit terminalx
+        </p>
+
+
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
